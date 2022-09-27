b203d6182a2e1d5cbcb6bc7d2f8f72dab82aab86
lozanofm <lozanofm@gmail.com>
2022-09-27 10:16:20 -0300

    avanzando con el primer proyecto react

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import  "./Navbar.css"
+
+const Navbar = () => {
+    return(
+        <nav class="navbar bg-primary">
+            
+            <h1>Crossfit</h1>
+            <div>
+                <button>Celulares</button>
+            </div>
+
+        </nav>
+
+    )
+}
+export default Navbar
